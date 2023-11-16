c8af8b4e042708b03e12a90b74561f72411f71ac
DariusSopeach <150965826+DariusSopeach@users.noreply.github.com>
2023-11-16 12:23:23 -0700

    Add files via upload

---
+++ formulario.css
@@ -0,0 +1,15 @@
+input{
+    border-radius: 15px;
+    padding: 2px;
+    }
+.select-container{
+    display: flex;
+    justify-content: center;
+    position: relative;
+    min-width: 250px;
+    height: 50px;
+}
+.select-box {
+    border-radius: 15px;
+    padding: 2px;
+}
